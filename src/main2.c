a86c54ea6b244e381dd3f10fa2907940acb52542
kavros <akavroulakis@gmail.com>
2019-04-03 17:31:56 +0100

    split code to functions

--- src/main2.c
+++ src/main2.c
@@ -23,7 +23,7 @@ int INITIAL_NUM_OF_INFECTED_SQUIRRELS = 4;
 bool 			isActorInitialized = false;
 long 			state;
 
-void initialiseSimulation()
+void initSimulationData()
 {
 	/*
 	* Every actor has an actor id which use for many reasons (ex. initialization).
@@ -72,33 +72,9 @@ void initialiseSimulation()
 	}
 }
 
-
-int main(int argc, char *argv[])
-{
-
-	// Every actor need to call the initialization function of the actor pattern.
-	AC_Init(argc,argv);
+void initFramework()
+{	
 	
-	/*
-	* Every actor has an actor id which use for many reasons (ex. initialization).
-	* Notice that the framework uses the actorId=0 internal so
-	* users need to initializate their actors starting from actor id equals to 1.
-	*/
-	int actorId = AC_GetActorId();
-
-	// Parsing command line arguments
-	int val = initCmdLineArgs(argc,argv,actorId);
-
-	// If the user run the programm using help flag we print a help message and terminate.
-	if(val == 1)
-	{
-		AC_Finalize();
-		return -1;
-	}
-
-	// Initialize squirrels, cells and global clock.
-	initialiseSimulation();
-
 	/**
 	* Initialize and pass to the framework:
 	* 	the total number of actors, 
@@ -111,9 +87,8 @@ int main(int argc, char *argv[])
 	int (*func_ptrs[3])() 		= {squirrelCode,cellCode,globalClockCode};
 	
 	// The actor framework will use **actorTypes_quantity** array to determine how many actors of each type will wake up. 
-	int actorTypes_quantity[3] 	= {NUM_OF_SQUIRRELS,NUM_OF_CELLS,1};
+	int actorTypes_quantity[3] = {NUM_OF_SQUIRRELS,NUM_OF_CELLS,1};	    
 	AC_SetActorTypes( numOfActors,actorTypes, actorTypes_quantity,func_ptrs);
-
 
 	/**
 	* Describes the message that actors using for comunication.
@@ -135,7 +110,36 @@ int main(int argc, char *argv[])
 	simulationMsgDisp[5] = offsetof(simulationMsg, y);
 	simulationMsgDisp[6] = offsetof(simulationMsg, command);
 	AC_SetActorMsgDataType(msgFields, msgDataTypeForEachField,blockLen,simulationMsgDisp);
+
+}
+
+int main(int argc, char *argv[])
+{
+	// Every actor need to call the initialization function of the actor pattern.
+	AC_Init(argc,argv);
 	
+	/*
+	* Every actor has an actor id which use for many reasons (ex. initialization).
+	* Notice that the framework uses the actorId=0 internal so
+	* users need to initializate their actors starting from actor id equals to 1.
+	*/
+	int actorId = AC_GetActorId();
+
+	// Parsing command line arguments
+	int val = initCmdLineArgs(argc,argv,actorId);
+
+	// If the user run the programm using help flag we print a help message and terminate.
+	if(val == 1)
+	{
+		AC_Finalize();
+		return -1;
+	}
+
+	// Initialize squirrels, cells and global clock with their starting data.
+	initSimulationData();
+	
+	// Set function pointers for actors and other necessary variables to the actor framework.
+	initFramework();
 
 	/**
 	* Starts simulation.
